13c53b5957bb6fec40ca6b06e2b34f760af89405
Jonathan Howard <Jon@Jons-MacBook.local>
2016-10-17 14:52:52 +0100

    Added numbers

---
+++ fundamentals/numbers.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+number1 = 1
+number2 = 2
+number3 = 10
+number4 = 16
+
+## Real basic operators
+print(number1 + number2) # <- Addition
+print(number3 - number2) # <- Subtraction
+print(number2 * number3) # <- Multiplication
+print(number4 / number3) # <- Division
+print((number4 + number2) * number3) # A bit of BODMAS
+
+## Powers
+print(number3 ** number4) # <- Raise number3 to the power of number4 (so 10^16)
+print(31.4159265359 * (10 ** -1)) # <- Raise original number to the power of 10^-1 (or divide by 10)
+
+## Division
+print(9 // 2) # Divide 9 by 2 (4.5) and round down (so answer is 4)
+print(9%2) # Divide 9 by 2 and display the number(s) after the decimal place
